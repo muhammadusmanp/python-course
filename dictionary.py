b933281d48e52a07bb286ac72d7a70ba0cebf8cf
Muhammad Usman <103430136+muhammadusmanp@users.noreply.github.com>
2025-04-24 15:52:56 +0500

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+mydic = {"name": "usman", "age": "26",
+         "phone": "03114967133"}
+
+print(mydic["name"])
+print(mydic["age"])
+print(mydic["phone"])
+print(mydic.keys())
+
+
+marks = {"science": 90, "English": 80, "urdu": 73,}
+type(marks)
+print(marks["urdu"])
+print(marks["English"])
+print(marks["science"])
+
+# adding item to the dictonary
+marks["Math"] = 66
+print(marks)
