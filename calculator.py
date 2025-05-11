02b79185cda192b67dc721d2806419fd3565df19
Muhammad Usman <103430136+muhammadusmanp@users.noreply.github.com>
2025-05-11 11:44:01 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+number1 = int(input("Enter a number : "))
+number2 = int(input("Enter a number : "))
+operator = input("Enter a operator : (+,-,*,/) ")
+
+if operator == "+" :
+  print(number1 + number2)
+
+if operator == "-" :
+  print(number1 - number2)
+
+if operator == "*" :
+  print(number1 * number2)
+
+if operator == "/" :
+  print(number1 / number2)
