ff98dadd05bb1e3da6c1a58b67386d01283df074
Muhammad Usman <103430136+muhammadusmanp@users.noreply.github.com>
2025-05-11 11:43:31 +0500

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,22 @@
+age = int(input("Enter your age :"))
+if age >= 18:
+  print("You can vote")
+else:
+  print("You can not vote")
+# 
+driver_age = int(input("Enter your age :"))
+print("Your age is :", driver_age)
+if driver_age >= 18:
+  print("you can drive the car")
+else:
+  print("You can not drive the car")
+# 
+marks = 49
+if marks >= 90:
+  print("A+ Grade")
+elif marks >= 75:
+  print("B Grade")
+elif marks >= 50:
+  print("Grade C")
+else:
+  print("You are Fail !")
