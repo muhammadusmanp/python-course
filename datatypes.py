e522ad1a05c808bae16440645ba1d7903e085213
Muhammad Usman <103430136+muhammadusmanp@users.noreply.github.com>
2025-05-11 15:15:54 +0500

    Add files via upload

--- datatypes.py
+++ datatypes.py
@@ -1,3 +1,10 @@
+#  Variable Naming Rules
+# Must start with a letter or underscore (_)
+# Can contain letters, numbers, and underscores (_)
+# Are case-sensitive (Name and name are different)
+# Can’t start with a number
+# Can’t use spaces
+# Can’t use special characters like @, $, %, !
 name = "usman"
 age = 26
 hight = 5.9
