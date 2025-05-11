1017aadbc4f1db6f3e23c418c92238166c0f5582
Muhammad Usman <103430136+muhammadusmanp@users.noreply.github.com>
2025-05-11 11:41:59 +0500

    Add files via upload

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,56 @@
+# addition of two numbers we will use + operator
+num1 = 20
+num2 = 3
+total = num1 + num2
+print(f"{num1} + {num2} = {total}")
+
+# for subtraction we use - operator 
+number1 = 20
+number2 = 2
+result = number1 - number2
+print(f"{number1} - {number2} = {result}")
+
+# for multiplication we use * in python
+
+value1 = 15
+value2 = 3
+total_values =  value1 * value2
+print(f"{value1} * {value2} = {total_values}")
+
+#  for division we use / in python
+orange = 10
+people = 3
+divide = orange / people
+print(f"{orange} / {people} {divide}")
+
+# // (Floor Division - gives the integer part we call it quotient 
+x = 10 
+y = 3
+z = x // y
+print(f"{x} // {y} = {z}")
+
+# calculate the remainder modolu
+print(16%5)
+
+# calculate the power
+print(2**4)  # 2 * 2 * 2 * 2
+
+# arithmetic operator
+a = 34
+b = 2
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a // b = ", a // b)
+print("a % b = ", a % b)
+print("a ** b =", a ** b)
+
+# conditional Operators 
+print(a > 4)
+print(a < 4)
+print(a >= 4)
+print(a <= 4)
+print(a == 4) # is a equal to 4 ?
+print(a == 34) # is a equal to 34 ?
+print(a != 34) # is a not equal to 34 ?
