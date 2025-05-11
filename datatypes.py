57a4a60b7c809478b5b9d280c4b0466efa9e88b3
Muhammad Usman <103430136+muhammadusmanp@users.noreply.github.com>
2025-05-11 15:31:09 +0500

    Add files via upload

--- datatypes.py
+++ datatypes.py
@@ -4,11 +4,12 @@
 # Are case-sensitive (Name and name are different)
 # Can’t start with a number
 # Can’t use spaces
-# Can’t use special characters like @, $, %, !
+# Can’t use special characters like @, $, %, 
+# A string is a collection of characters enclosed within quotation marks (‘ ‘ or ” “).
 name = "usman"
-age = 26
-hight = 5.9
-student = True
+age = 26  # An integer is a whole number without any decimal point.
+hight = 5.9  # A float is a number that has a decimal point.
+student = True # this is bool data type True or False values only.
 print(name,type(name))
 print(age,type(age))
 print(hight,type(hight))
