f93857d40fa66b138274f619f5c042216758dfd5
Muhammad Usman <103430136+muhammadusmanp@users.noreply.github.com>
2025-05-11 14:20:33 +0500

    Add files via upload

---
+++ first_programme.py
@@ -0,0 +1,7 @@
+# This code will print my name and student ID
+name = "usman"
+student_id = "0044"
+print(f"my name is {name} and my id is {student_id}")
+#  print wellcome to the world of python programming
+wellcome_note = "wellcome to the world of python programming"
+print(f"{wellcome_note}")
