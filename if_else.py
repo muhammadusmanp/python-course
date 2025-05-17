f00ceb466b45ec0c25fe9202a4467e069144e0bb
Muhammad Usman <103430136+muhammadusmanp@users.noreply.github.com>
2025-05-17 21:40:38 +0500

    Add files via upload

--- if_else.py
+++ if_else.py
@@ -1,17 +1,18 @@
+# this block of code check the voter age of print either you can vote or not.
 age = int(input("Enter your age :"))
 if age >= 18:
   print("You can vote")
 else:
   print("You can not vote")
-# 
+#  
 driver_age = int(input("Enter your age :"))
 print("Your age is :", driver_age)
 if driver_age >= 18:
   print("you can drive the car")
 else:
   print("You can not drive the car")
-# 
-marks = 49
+
+marks = 60
 if marks >= 90:
   print("A+ Grade")
 elif marks >= 75:
@@ -20,3 +21,15 @@ elif marks >= 50:
   print("Grade C")
 else:
   print("You are Fail !")
+
+# Ask the user how they're feeling:
+
+user_mood = input("what are you feeling :")
+if user_mood == "happy":
+    print("that is great to hear")
+elif user_mood == "sad":
+    print("i hope you day gets better")
+elif user_mood == "bored":
+    print("go for a walk")
+else:
+    print("Thanks for sharing your mood")
